c4f9609651e73b206d596367994aa0e26336e5ff
Gabriel Azocar <gabriel@startnet.co>
2017-06-09 00:35:18 -0400

    Quicksort

---
+++ Auxiliares/SolAux10/Quicksort/Pedido.java
@@ -0,0 +1,11 @@
+public class Pedido {
+  int nOrden;
+  String tipoPizza;
+  String direccion;
+  
+  public Pedido(int n, String t, String d){
+      this.nOrden = n;
+      this.tipoPizza = t;
+      this.direccion = d;
+  }
+}
---
+++ Auxiliares/SolAux10/Quicksort/QuicksortExample.java
@@ -0,0 +1,82 @@
+public class QuicksortExample {
+  
+  public static int randomPivot(int largo, int izq, int der) {
+    return izq + ((int) Math.random() * (largo)) / (der - izq + 1);
+  }
+  
+  public static int medianaPivot(Pedido pedidos[], int izq, int der) {
+    int med = (izq + der) / 2;
+    
+    if(pedidos[izq].nOrden <= pedidos[med].nOrden) {
+      if(pedidos[der].nOrden <= pedidos[izq].nOrden) { //izq < med y der < izq
+        return izq;
+      } else { //izq < med y izq < der
+        return (pedidos[med].nOrden <= pedidos[der].nOrden) ? med : der;
+      }
+    } else {
+      if(pedidos[der].nOrden <= pedidos[med].nOrden) { //med < izq y der < med
+        return med;
+      } else { //med < izq y med < der
+        return (pedidos[izq].nOrden <= pedidos[der].nOrden) ? izq : der;
+      }
+    }
+  }
+  
+  public static void quicksort(Pedido pedidos[], int izq, int der) {
+    
+    //int pIndex = randomPivot(pedidos.length, izq, der);
+    int pIndex = medianaPivot(pedidos, izq, der);
+    
+    Pedido pivote = pedidos[pIndex];
+
+    int i = izq;
+    int j = der;
+    Pedido aux;
+   
+    while (i < j) {
+       while(pedidos[i].nOrden <= pivote.nOrden && i < j) i++;
+       
+       while(pivote.nOrden < pedidos[j].nOrden) j--;         
+       
+       if (i < j) {                                            
+           aux = pedidos[i];                  
+           pedidos[i] = pedidos[j];
+           pedidos[j] = aux; 
+       }
+     }
+    
+     pedidos[izq] = pedidos[j];
+     pedidos[j] = pivote;
+
+     
+     if(izq < j - 1)
+        quicksort(pedidos, izq, j - 1);
+     
+     if(j + 1 <der)
+        quicksort(pedidos, j + 1, der);
+  }
+
+  public static void main(String[] args) {
+    Pedido a = new Pedido(1, "Italiana", "Beauchef 850");
+    Pedido b = new Pedido(12, "Pina", "Beauchef 851");
+    Pedido c = new Pedido(4, "Napolitana", "Providencia 1134");
+    Pedido d = new Pedido(54, "Tres carnes", "Av. Matta 201");
+    Pedido e = new Pedido(2, "Tres quesos", "Santa Isabel 399");
+    Pedido f = new Pedido(3, "Hawaiana", "Marin 30");
+    Pedido g = new Pedido(6, "Vegetariana", "Nevada 25");
+    Pedido h = new Pedido(10, "Completa", "Alameda 50");
+    
+    Pedido pedidos[] = {a, b, c, d, e, f, g, h};
+    for(int i = 0; i < pedidos.length; i++) {
+      System.out.print(pedidos[i].nOrden + " ");
+    }
+    System.out.println();
+    
+    quicksort(pedidos, 0, pedidos.length - 1);
+    
+    for(int i = 0; i < pedidos.length; i++) {
+      System.out.print(pedidos[i].nOrden + " ");
+    }
+  }
+
+}
